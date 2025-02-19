27874f96e4d1fa01092d565e1ffc07ec67b1909b
AlexandreFB2003 <afborges2003@gmail.com>
2025-02-19 18:17:37 -0300

    final

--- src/app/components/buttons/buttons.tsx
+++ src/app/components/buttons/buttons.tsx
@@ -1,12 +1,23 @@
+"use client";
+
 import "./buttons.scss"
 
 export function Buttons() {
+
+  const handleDownload = () => {
+    const pdfUrl = "/cv-back-end.pdf"; // Caminho do arquivo
+    const link = document.createElement("a");
+    link.href = pdfUrl;
+    link.download = "CV-Alexandre.pdf";
+    link.click();
+};
+
     return (
         <div className="buttons">
           <div className="social">
 
           </div>
-          <button className="curriculo">Currículo</button>
+          <button className="curriculo" onClick={handleDownload}>Currículo</button>
         </div>
     )
 }
